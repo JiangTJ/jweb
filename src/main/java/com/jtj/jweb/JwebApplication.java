ea0dfdaa3b9fa0a6e490034e3524de90f00b7bf5
jiangtj <116749895@qq.com>
2018-05-30 14:49:32 +0800

    init

---
+++ src/main/java/com/jtj/jweb/JwebApplication.java
@@ -0,0 +1,21 @@
+package com.jtj.jweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class JwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwebApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        return "index.html";
+    }
+
+}
